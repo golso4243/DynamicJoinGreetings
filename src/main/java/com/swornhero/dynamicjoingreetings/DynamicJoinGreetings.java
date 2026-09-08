e8cee6acce66ebd3920274289642ee23c6f944fd
Gage Olson <gage.a.olson@gmail.com>
2026-09-08 14:33:59 -0500

    chore: configure server-only mod foundation

--- src/main/java/com/swornhero/dynamicjoingreetings/DynamicJoinGreetings.java
+++ src/main/java/com/swornhero/dynamicjoingreetings/DynamicJoinGreetings.java
@@ -1,27 +1,17 @@
 package com.swornhero.dynamicjoingreetings;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.resources.Identifier;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DynamicJoinGreetings implements ModInitializer {
+public final class DynamicJoinGreetings implements ModInitializer {
 	public static final String MOD_ID = "dynamic-join-greetings";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Dynamic Join Greetings initialized.");
 	}
 
 	public static Identifier id(String path) {
